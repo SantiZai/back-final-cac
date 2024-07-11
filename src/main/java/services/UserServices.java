40f655261bb69caeb5cdb1fe637d108703704ce7
SantiZai <santyagozaidan@gmail.com>
2024-07-11 19:34:16 -0300

    user services and controllers added

--- src/main/java/services/UserServices.java
+++ src/main/java/services/UserServices.java
@@ -1,6 +1,16 @@
 package services;
 
+import domain.models.User;
 import infrastructure.IPersistence;
+import infrastructure.database.MySQLPersistenceImpl;
 
 public class UserServices implements IPersistence {
+
+    private IPersistence persistence = new MySQLPersistenceImpl();
+
+    @Override
+    public void saveUser(User user) {
+        persistence.saveUser(user);
+    }
+
 }
