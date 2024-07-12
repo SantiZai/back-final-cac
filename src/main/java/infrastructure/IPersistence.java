0a1c59802d1c66afe1e486ca993f6d6a51055933
SantiZai <santyagozaidan@gmail.com>
2024-07-11 23:31:16 -0300

    find all users persistence

--- src/main/java/infrastructure/IPersistence.java
+++ src/main/java/infrastructure/IPersistence.java
@@ -2,9 +2,13 @@ package infrastructure;
 
 import domain.models.User;
 
+import java.util.ArrayList;
+
 public interface IPersistence {
 
     void saveUser(User user);
+
+    ArrayList<User> findAllUsers();
 
     User findUserByUsername(String username);
 
